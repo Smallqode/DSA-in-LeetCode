6dedde0775465bcb335d67d41d6dd6cf412a99f3
Smallqode <saugataghosh23@gmail.com>
2025-08-16 22:33:01 +0530

    Time: 3 ms (74.24%) | Memory: 25.6 MB (46.41%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def helper(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        k = k % len(nums)
+        helper(0, len(nums) - 1)
+        helper(0, k - 1)
+        helper(k, len(nums) - 1)
+        return nums

1511273c10a18de32b46b07203da8f35177a4ed7
Smallqode <saugataghosh23@gmail.com>
2025-11-05 23:36:19 +0530

    Time: 3 ms (47.59%) | Memory: 17.8 MB (48.26%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        max_heap = [[-cnt, char] for char, cnt in count.items()]
+        heapify(max_heap)
+        prev = None
+        res = ""
+        while max_heap or prev:
+            if prev and not max_heap:
+                return ""
+            cnt, char = heappop(max_heap)
+            res += char
+            cnt += 1
+
+            if prev:
+                heappush(max_heap, prev)
+                prev = None
+            if cnt != 0:
+                prev = [cnt, char]
+        return res

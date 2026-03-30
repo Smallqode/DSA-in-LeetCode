cae6e3f817fe3fb2007bdcb5c94d9e8a2bb3ddc8
Smallqode <saugataghosh23@gmail.com>
2026-03-30 23:55:15 +0530

    Time: 223 ms (11.94%) | Memory: 20.2 MB (68.66%) - LeetSync

---
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        def get_f(s):
+            parity = [Counter(), Counter()]
+            for i in range(len(s)):
+                parity[i % 2][s[i]] += 1
+            return parity
+        return get_f(s1) == get_f(s2)
